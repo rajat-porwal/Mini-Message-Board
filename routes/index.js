9df9d3ba871ef302e9480a3c2a57f839842888f8
Rajat Porwal <rajatporwal@protonmail.com>
2024-05-27 05:54:33 +0530

    add routes for index and named index and new-message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Diksha",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Rajat",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = {router, messages};
---
+++ routes/new-message.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const { messages } = require("./index.js");
+
+router.get("/", function (req, res, next) {
+  res.render("new-message", { title: "New Message", sent: false, error: null });
+});
+
+
+router.post("/new", (req, res, next) => {
+  const message = {
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+    error: null,
+  };
+  if (!message.text || !message.user) {
+    return res.render("new-message", {
+      title: "New Message",
+      sent: false,
+      error: "All fields are required",
+    });
+  }
+  messages.push(message);
+  console.log(
+    `Message from ${message.user} : "${message.text}" has been posted`
+  );
+  res.redirect("/");
+});
+
+module.exports = router;
